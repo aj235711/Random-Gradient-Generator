3f8fe1921375add1e12f0c762acfd1f40e6f8afe
aj235711 <66076781+aj235711@users.noreply.github.com>
2020-06-17 22:10:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+var h5 = document.querySelector("h5");
+var css = getComputedStyle(body);
+var bg = css.background;
+h5.innerHTML = bg;
+
+ 
+button.addEventListener("click", createBG)
+
+function createBG() {
+	var arr = [[], [], []];
+	for(var i = 0; i < 3; i++) {
+		// if(!arr[i]) {
+		// 	arr[i] = [];
+		// }
+		for(var j = 0; j < 3; j++) {
+			arr[i][j] = Math.floor((Math.random() * 255) + 1);
+		}
+	}
+	body.style.background = "linear-gradient(to right, rgb(" + arr[0][0] + ", " + arr[0][1] + ", " + arr[0][2] + "), rgb("
+	+ arr[1][0] + ", " + arr[1][1] + ", " + arr[1][2] + "), rgb("
+	+ arr[2][0] + ", " + arr[2][1] + ", " + arr[2][2] + "))";
+	h5.innerHTML = body.style.background + ";";
+}
